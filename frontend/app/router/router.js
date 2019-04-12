83d31a8b31411945144deb8f2eae8d32b675873d
Edward Toebben <ejtn78@mail.missouri.edu>
2019-04-12 15:51:20 -0500

    Added new tab called lines

--- frontend/app/router/router.js
+++ frontend/app/router/router.js
@@ -17,6 +17,7 @@ import MainControls from '../components/MainControls.vue'
 import AugurHeader from '../components/AugurHeader.vue'
 import Tabs from '../components/Tabs.vue'
 import TableView from '../components/TableView.vue'
+import LinesCard from '../components/LinesCard.vue'
 
 let routes = [
         {path: '/', component: AugurCards,
@@ -107,6 +108,7 @@ let routes = [
               content: GitCard
             }
           },
+        
           {
             path: "overview",
             name: "overview",
@@ -115,6 +117,16 @@ let routes = [
               tabs: Tabs,
               controls: MainControls,
               content: OverviewCard
+            }
+          },
+              {
+            path: "lines",
+            name: "lines",
+            components: {
+              header: AugurHeader,
+              tabs: Tabs,
+              controls: MainControls,
+              content: LinesCard
             }
           },
         ]
